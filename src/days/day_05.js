9c6cb1e26f37d19b778b11cc894d4f6a1dc741fa
Viktor Ceder <viktor.ceder@mpyadigital.com>
2023-12-08 07:34:02 +0100

    make another atempt on day 05 part 2

--- src/days/day_05.js
+++ src/days/day_05.js
@@ -9,14 +9,13 @@ const buildConversionTable = (data) =>
       .split("\n")
       .filter((row) => row !== "")
       .map((value) => value.split(" ").map((number) => Number(number)))
-      .reduce((conversionTable, [output, input, range]) => {
-        const table = { start: input, end: input + range, output };
-        // for (let index = 0; index < length; index++) {
-        //   table[source + index] = destination + index;
-        // }
-        // console.log(table);
-        return [...conversionTable, table];
-      }, []);
+      .reduce(
+        (conversionTable, [output, input, range]) => [
+          ...conversionTable,
+          { start: input, end: input + range, output },
+        ],
+        []
+      );
 
     return [
       ...maps,
@@ -59,6 +58,91 @@ const convertSeedToLocation = (
   return value;
 };
 
+const convertSeedRangeToLocationRange = (startRange, maps) => {
+  const toDo = [{ ...startRange, type: "seed" }];
+  const done = [];
+  while (toDo.length > 0) {
+    let range = toDo.shift();
+
+    while (true) {
+      const table = maps.find(({ from }) => from === range.type);
+      if (!table || range.type === "location") {
+        done.push({ ...range });
+        break;
+      }
+      const conversion = table.conversion.find(
+        ({ start, end }) => start <= range.start && range.start < end
+      );
+
+      if (conversion) {
+        if (conversion.end >= range.end) {
+          range.start = conversion.output + range.start - conversion.start;
+          range.end = conversion.output + range.end - conversion.start;
+        } else {
+          toDo.push({
+            start: conversion.end,
+            end: range.end,
+            type: range.type,
+          });
+
+          range.start = conversion.output + range.start - conversion.start;
+          range.end = conversion.output + conversion.end - conversion.start + 1;
+        }
+      }
+      range.type = table.to;
+    }
+  }
+  return done;
+};
+
+const convertSeedRangeToLocationRangeRecursive = (
+  seedRange, // {start, end}
+  maps,
+  type = "seed"
+) => {
+  const table = maps.find(({ from }) => from === type);
+  // console.log("going down:", type);
+  if (table === undefined) {
+    // console.log(`bottom found, returning ${seedRange}`);
+    return seedRange;
+  }
+
+  const conversion = table.conversion.find(({ start, end }) => {
+    // console.log(start <= seedRange.start);
+    // console.log(seedRange.start <= end);
+    return start <= seedRange.start && seedRange.start <= end;
+  });
+  // console.log(conversion);
+  if (conversion === undefined) {
+    // console.log("no conversion found");
+    return convertSeedRangeToLocationRangeRecursive(seedRange, maps, table.to);
+  }
+  if (conversion.end >= seedRange.end) {
+    // console.log("whole range was in conversion");
+    return convertSeedRangeToLocationRangeRecursive(
+      {
+        start: conversion.output + seedRange.start - conversion.start,
+        end: conversion.output + seedRange.end - conversion.start,
+      },
+      maps,
+      table.to
+    );
+  } else {
+    const first = {
+      start: conversion.output + seedRange.start - conversion.start,
+      end: conversion.output + conversion.end - conversion.start,
+    };
+    const second = { start: conversion.end, end: seedRange.end };
+    // console.log("splitting range");
+    // console.log(first);
+    // console.log(second);
+    return [
+      convertSeedRangeToLocationRangeRecursive(first, maps, conversion.to),
+      convertSeedRangeToLocationRangeRecursive(second, maps, type),
+    ];
+  }
+};
+
 export const partA = (input) => {
   const data = input.join("\n").split("\n\n");
   const maps = buildConversionTable(data);
@@ -97,11 +181,17 @@ export const partB = (input) => {
   const maps = buildConversionTable(data);
   const seeds = getSeeds(data).reduce((seeds, seed, i) => {
     if (i % 2) {
-      seeds.at(-1).stop = seeds.at(-1).start + seed;
+      seeds.at(-1).end = seeds.at(-1).start + seed;
       return seeds;
     }
-    return [...seeds, { start: seed, stop: 0 }];
+    return [...seeds, { start: seed, end: 0 }];
   }, []);
-  return false;
+  return (
+    seeds
+      .map((seedRange) => convertSeedRangeToLocationRange(seedRange, maps))
+      .flat()
+      // .map(({ start }) => start)
+      .sort((a, b) => a.start - b.start)[0].start
+  ); // 107430936 <- too high
   // return maps;
 };
